854716bb1ff0dff5ce86e52cf8baaa858d882d12
Andreas Abel <abel@cs.uni-saarland.de>
2021-07-28 00:13:19 +0200

    heatmaps

--- avgError.py
+++ avgError.py
@@ -37,16 +37,16 @@ def main():
    parser.add_argument('csv', help="csv file")
    parser.add_argument('col1', type=int)
    parser.add_argument('col2', type=int, nargs='?')
-   parser.add_argument('-heatmap', action='store_true')
+   parser.add_argument('-heatmap', nargs=2, help='1st arg: filename, 2nd arg: title')
    parser.add_argument('-showDiff', action='store_true')
    parser.add_argument('-metrics', default='MAPE,kendall,pearson,spearman')
    parser.add_argument('-round', action='store_true')
    parser.add_argument('-baselineUnroll', action='store_true')
    parser.add_argument('-baselineLoop', action='store_true')
    parser.add_argument('-issueWidth', type=int, default=4)
-   parser.add_argument('-CPI', type=int, help="normalize TP to CPI; the parameter must contain the column index for the assembler code")
+   parser.add_argument('-CPI', type=int, help='normalize TP to CPI; the parameter must contain the column index for the assembler code')
    args = parser.parse_args()
-      
+
    with open(args.csv, 'r') as f:
       lines = f.read().splitlines()
 
@@ -63,7 +63,7 @@ def main():
       tp2L = [max(100, mul*(l.count(';'))) for l in lines]
       #print(tp2L)
    else:
-      tp2L = getColumn(lines, args.col2, args.CPI)   
+      tp2L = getColumn(lines, args.col2, args.CPI)
 
    if not args.heatmap:
       if args.showDiff:
@@ -86,7 +86,7 @@ def main():
             print('{:.4f}'.format(tau[0],4))
          else:
             print(tau)
-      if 'pearson' in args.metrics: 
+      if 'pearson' in args.metrics:
          pearson = scipy.stats.pearsonr(tp1L, tp2L)
          if args.round:
             print('{:.4f}'.format(pearson[0]))
@@ -101,7 +101,7 @@ def main():
    else:
       start = 0
       end = 1000
-      
+
       data = [[0]*100 for _ in range(0,100)]
 
       for tp1, tp2 in zip(tp1L, tp2L):
@@ -110,7 +110,6 @@ def main():
          if tp1 < 100 and tp2 < 100:
             data[tp2][tp1] += 1
 
-
       start = 0
       end = 1000
       bins = np.arange(5, end + 6, 10)
@@ -118,20 +117,16 @@ def main():
       heatmap, _, _ = np.histogram2d(tp1L, tp2L, bins=bins)
 
       #fig = plt.figure()
-      
-      
       #heatMap = sns.heatmap(heatmap.T, norm=LogNorm(), cmap=sns.cm.rocket_r, vmin=1, vmax=50000)
       #heatMap.invert_yaxis()
-      
       #plt.plot(np.linspace(start, end, 1000), np.linspace(start, end, 1000), 'k--', alpha=0.2)
       #plt.grid()
       #ax = fig.gca()
       #ax.set_xticks(np.arange(5.5, 106, 10))
       #ax.set_yticks(np.arange(5.5, 106, 10))
       #plt.show()
-      
       #exit(0)
-      
+
       matplotlib.use("pgf")
       matplotlib.rcParams.update({
           "pgf.texsystem": "pdflatex",
@@ -140,42 +135,30 @@ def main():
           'pgf.rcfonts': False,
       })
 
-      '''
-      for b in range(len(bins) - 1):
-         print(heatmap[b, :])
-         div = heatmap[b, :].sum() or 1
-         heatmap[b, :] /= div
-      '''
       # based on Ithemal's Figures.ipynb
 
       extreme = max(map(abs, (heatmap.T.max(), heatmap.T.min())))
-      # extreme = 1
       extent = [start+5, end+5, start+5, end+5]
-      lognorm = LogNorm()
-      #lognorm = None
+      lognorm = LogNorm(vmin=1, vmax=50000)
       clim = (-extreme, extreme)
-      print(heatmap.T)
 
       cmap = plt.get_cmap('bwr')
 
       fig = plt.figure(figsize=(8, 6))
       ax = fig.add_subplot(1, 1, 1)
-      plt.imshow(heatmap.T, cmap=sns.cm.rocket_r, norm=lognorm, extent=extent, origin='lower', vmin=1, vmax=50000)
+      plt.imshow(heatmap.T, cmap=sns.cm.rocket_r, norm=lognorm, extent=extent, origin='lower')
       plt.plot(np.linspace(start+5, end+5, 1000), np.linspace(start+5, end+5, 1000), 'k--', alpha=0.2)
-      #plt.grid()
       cbar = plt.colorbar()
-      #plt.tick_params(labelsize=10)
-      #cbar.ax.tick_params(labelsize=10)
-      plt.xticks(np.arange(start, end+1, 100), labels=range(0, 11, 1))
-      plt.yticks(np.arange(start, end+1, 100), labels=range(0, 11, 1))
-      ax.set_xlabel('Measured Throughput', fontsize=14)
-      ax.set_ylabel('Predicted Throughput', fontsize=14)
-      ax.set_title('Haswell', fontsize=16)
-      #plt.show()
+      for t in cbar.ax.get_yticklabels():
+         t.set_fontsize(14)
+      plt.xticks(np.arange(start, end+1, 100), labels=range(0, 11, 1), fontsize=14)
+      plt.yticks(np.arange(start, end+1, 100), labels=range(0, 11, 1), fontsize=14)
+      ax.set_xlabel('Measured Throughput', fontsize=20)
+      ax.set_ylabel('Predicted Throughput', fontsize=20)
+      ax.set_title(args.heatmap[1], fontsize=20)
 
-      
-      plt.savefig('histogram.pgf')
+      plt.savefig(args.heatmap[0], bbox_inches='tight')
 
-   
+
 if __name__ == "__main__":
     main()
