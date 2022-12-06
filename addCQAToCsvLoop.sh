d38e823a5e53b68ea9ad0723dd6c50eab55a351a
Andreas Abel <abel@cs.uni-saarland.de>
2022-12-06 16:04:43 +0100

    CQA 2.16

--- addCQAToCsvLoop.sh
+++ addCQAToCsvLoop.sh
@@ -7,7 +7,7 @@ run_CQA() {
    asmFile=`mktemp --tmpdir asm.XXXXXXXXXX`
    echo $line | awk -F, '{print $1$2$3}' | fold -w 2 | awk '{print ".byte 0x" $1}' > $asmFile
    as $asmFile -o $codeFile
-   tp=`../maqao.intel64.2.15.0/maqao.intel64 cqa $codeFile fct-loops=.* uarch=$uarch --confidence-levels=expert | grep 'each iteration of the binary loop takes' | head -n 1 | sed -E 's/.*takes (.*) cycles.*/\1/' | tr -d "."`
+   tp=`../maqao.intel64.2.16.0/maqao.intel64 cqa $codeFile fct-loops=.* uarch=$uarch --confidence-levels=expert | grep 'each iteration of the binary loop takes' | head -n 1 | sed -E 's/.*takes (.*) cycles.*/\1/' | tr -d "."`
    rm $codeFile $asmFile
    echo "$line,$tp"
 }
